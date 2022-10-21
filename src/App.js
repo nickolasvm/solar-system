90e73eb7ebdaec3f452ec790027ca68e0e6114c9
nickolasvm <nickolas64vm@gmail.com>
2022-10-21 16:14:05 -0300

    Criados componentes Missions e MissionCard & renderizados as missões na tela

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
-import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <Title headLine="Planetas" />
         <SolarSystem />
+        <Missions />
       </>
     );
   }
